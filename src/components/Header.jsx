cc604301e7f900cb54d91a802458cf24a3f4a4fa
Hazael Torres <hazaeltorres09@gmail.com>
2025-11-07 19:50:19 -0600

    Explicación jsx y Fragments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
 export default function Header() {
-  return <h1>Desde header</h1>;
+  const name = "Hazael";
+
+  return (
+    <>
+      <h1>Desde header</h1>
+      <p>Hola : {name}</p>
+    </>
+  );
 }
