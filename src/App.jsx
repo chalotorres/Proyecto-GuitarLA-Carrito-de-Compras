d97b9cf2b99b586a6b85ff03e5a5063c637930cd
Hazael Torres <hazaeltorres09@gmail.com>
2025-11-07 20:35:55 -0600

    introducción a Hooks y States

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
 
 function App() {
+  // State
+  const [auth, setAuth] = useState(false);
+
   return (
     <>
       <Header />
